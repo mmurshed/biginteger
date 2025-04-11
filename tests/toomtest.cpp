cc222dd1e0b37412cd18f74e85a3f8c710ce2f3e
xfab-max <max@xfabric.ai>
2025-04-10 17:42:50 -0700

    toom-cook test

---
+++ tests/toomtest.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+                          main.cpp  -  description
+                             -------------------
+    begin                : Thu Aug 29 02:15:55 BDT 2002
+    copyright            : (C) 2002 by S. M. Mahbub Murshed
+    email                : murshed@gmail.com
+ ***************************************************************************/
+ 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <strstream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+#include "../BigInteger.h"
+#include "../BigIntegerIO.h"
+#include "../BigIntegerParser.h"
+#include "../BigIntegerComparator.h"
+#include "../algorithms/toomcook/ToomCookMultiplication.h"
+#include "../algorithms/toomcook2/ToomCookMultiplication2.h"
+#include "../algorithms/toomcook2/ToomCookMultiplication2a.h"
+#include "../algorithms/toomcookmemoptim/ToomCookMultiplicationMemOptimized.h"
+#include "../algorithms/classic/ClassicMultiplication.h"
+
+using namespace BigMath;
+
+
+int main(int argc, char *argv[])
+{
+/*
+  if(argc < 3)
+  {
+    cerr << "Usage: toomtest [INPUT] [OUTPUT] [ANSWER]" << endl;
+    return 1;
+  }
+
+  if(!freopen(argv[1],"rt",stdin))
+    return 1;
+
+  if(!freopen(argv[2],"wt",stdout))
+    return 1;
+
+  ifstream ansFile(argv[3]);
+*/
+
+  if(!freopen("input.txt","rt",stdin))
+    return 1;
+
+  if(!freopen("output.txt","wt",stdout))
+    return 1;
+
+  ifstream ansFile("answer.txt");
+
+  long DATA = 1;
+
+  char op;
+
+
+  cerr << "Data,Toom-Cook,Results Digit" << endl;
+
+  while(true)
+  {
+    BigInteger a, b;
+   
+    cin >> a >> op >> b;
+
+    // vector<DataT> res1 = CommonAlgorithms::ShiftLeft(a.GetInteger(), 2);
+
+    // cerr << a << endl;
+    // cerr << res1 << endl;
+
+    string line;
+    std::getline(ansFile, line);
+    BigInteger ans = BigIntegerParser::Parse(line.c_str());
+
+    /*
+    vector<DataT> ans = ClassicMultiplication::Multiply(
+      a.GetInteger(),
+      b.GetInteger(),
+      BigInteger::Base());
+    */
+    
+    clock_t start = clock();
+    ToomCookMultiplication2 tcm;
+    vector<DataT> rToom = tcm.Multiply(
+      a.GetInteger(),
+      b.GetInteger(),
+      BigInteger::Base());
+    clock_t end = clock();
+    double timeTakenToomCook = (double)(end - start) / CLOCKS_PER_SEC;
+
+    // cout << ans << endl;
+    cout << rToom << endl;
+
+    cerr.setf(ios::showpoint);
+    cerr << DATA << "," << timeTakenToomCook << "," << rToom.size() << endl;
+
+    int cmp = BigIntegerComparator::CompareTo(rToom, ans.GetInteger());
+    if(cmp != 0)
+    {
+      cerr << "Toom-Cook algorithm failed." << endl;
+      return cmp;
+    }
+
+    DATA++;
+  
+    if(cin.eof())
+      break;
+  }
+
+  return EXIT_SUCCESS;
+}
+
