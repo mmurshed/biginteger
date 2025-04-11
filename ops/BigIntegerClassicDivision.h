2ad4d110da7e135bccd729689c1171feb9fa34a9
xfab-max <max@xfabric.ai>
2025-04-11 14:05:42 -0700

    introduce classic operations

---
+++ ops/BigIntegerClassicDivision.h
@@ -0,0 +1,69 @@
+/**
+ * BigInteger Class
+ * Version 9.0
+ * S. M. Mahbub Murshed (murshed@gmail.com)
+ */
+
+#ifndef BIGINTEGER_CLASSIC_DIVISION
+#define BIGINTEGER_CLASSIC_DIVISION
+
+#include <utility>
+#include <vector>
+using namespace std;
+
+#include "../BigInteger.h"
+#include "../algorithms/classic/ClassicDivision.h"
+
+namespace BigMath
+{
+  class BigIntegerClassicDivision
+  {
+  public:
+    static pair<BigInteger, BigInteger> DivideAndRemainder(BigInteger const &a, BigInteger const &b)
+    {
+      if (a.IsZero() || b.IsZero())
+      {
+        BigInteger q = BigInteger();
+        return make_pair(q, q); // case of 0
+      }
+
+      Int cmp = BigIntegerComparator::CompareTo(a.GetInteger(), b.GetInteger());
+      if (cmp == 0)
+      {
+        vector<DataT> one(1, 1); // size 1, value 1
+        BigInteger q = BigInteger(one, a.IsNegative() || b.IsNegative());
+        BigInteger r = BigInteger();
+        return make_pair(q, r); // case of 0 // case of a/a
+      }
+      else if (cmp < 0)
+      {
+        BigInteger q = BigInteger();
+        BigInteger r = BigInteger(a.GetInteger(), a.IsNegative() || b.IsNegative());
+        return make_pair(q, r); // case of a < b
+      }
+
+      // Now: a > b
+      pair<vector<DataT>, vector<DataT>> result = ClassicDivision::DivideAndRemainder(a.GetInteger(), b.GetInteger(), BigInteger::Base());
+
+      // Convert to BigInteger
+      BigInteger q = BigInteger(result.first, a.IsNegative() || b.IsNegative());
+      BigInteger r = BigInteger(result.second, a.IsNegative() || b.IsNegative());
+
+      return make_pair(q, r);
+    }
+  };
+
+  // Divides Two BigInteger
+  BigInteger operator/(BigInteger const &a, BigInteger const &b)
+  {
+    return BigIntegerClassicDivision::DivideAndRemainder(a, b).first;
+  }
+
+  BigInteger operator%(BigInteger const &a, BigInteger const &b)
+  {
+    return BigIntegerClassicDivision::DivideAndRemainder(a, b).second;
+  }
+
+}
+
+#endif
